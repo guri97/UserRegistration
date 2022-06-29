601c3af987201eb4f2ad0f0ed4cade7da246c66f
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-29 21:57:48 +0530

    [gurpreet] UC1_UserFirstName

---
+++ UserRegistration/TestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+namespace UserRegistrationTestAndRegex
+{
+    public class Tests
+    {
+        UserRegistrationDetails user;
+        [SetUp]
+        public void Setup()
+        {
+            user = new UserRegistrationDetails();
+        }
+
+        [Test]
+        public void FirstName()
+        {
+            // Arrange 
+            bool expected = true;
+            string name = "Gurpreet";
+            UserRegistrationDetails user = new UserRegistrationDetails();
+
+            // Act
+            bool result = user.ValidateFirstName(name);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace UserRegistrationTestAndRegex
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            do
+            {
+                Console.WriteLine("Welcome To N unit testing and Exception handling For User Registration Problem");
+                Console.Write("\nPlease enter user details\n");
+                UserRegistrationDetails user = new UserRegistrationDetails();
+                menu(user);
+                Console.Write("\nDo you want to continue?(y/n)    ");
+                ch = Convert.ToChar(Console.ReadLine());
+                Console.Clear();
+            } while (ch == 'y');
+
+            Console.ReadKey();
+        }
+
+        private static void menu(UserRegistrationDetails user)
+        {
+
+            Console.Write("\nFirst Name:     ");
+            string firstName = Console.ReadLine();
+            Console.Write("\n=====Result======\n");
+            bool checkFirstName = user.ValidateFirstName(firstName);
+            Console.WriteLine("\n{0}\n{1}\n{2}\n{3}\n{4} ",
+            (checkFirstName) ? "First name is valid" : "First name is invalid");
+        }
+    }
+}
